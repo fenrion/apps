88adea88a6286ff46f758fa960e6f0507b7446d6
Maksim <fenrion301193@gmail.com>
2023-04-13 17:34:10 +0300

    bla bla

---
+++ Project3Solution-master/Project3/src/main/java/ru/kopylov/springcourse/project3/repositories/SensorRepository.java
@@ -0,0 +1,13 @@
+package ru.kopylov.springcourse.project3.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import ru.kopylov.springcourse.project3.models.Sensor;
+
+import java.util.Optional;
+
+
+@Repository
+public interface SensorRepository extends JpaRepository<Sensor, String> {
+    Optional<Sensor> findByName(String name);
+}
---
+++ Project3Solution-master/Project3/src/main/java/ru/kopylov/springcourse/project3/util/MeasurementException.java
@@ -0,0 +1,8 @@
+package ru.kopylov.springcourse.project3.util;
+
+
+public class MeasurementException extends RuntimeException {
+    public MeasurementException(String msg) {
+        super(msg);
+    }
+}
